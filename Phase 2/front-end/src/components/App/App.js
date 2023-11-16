cb33f7b2580a6145fe23f3e41027ede1e22d3da4
mlec01 <emily.y.chiu@ryerson.ca>
2023-11-16 04:13:29 -0500

    create login form

--- Phase 2/front-end/src/components/App/App.js
+++ Phase 2/front-end/src/components/App/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/dashboard' element={<Dashboard />}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="wrapper">
+      {/* <h1>Hello World!</h1> */}
+      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
