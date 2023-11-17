145035dd7163806603b651900ad6b85c7b0412fc
allenAbi <a1allen@ryerson.ca>
2023-11-17 00:37:30 -0500

    Add header

--- Phase 2/front-end/src/components/App/App.js
+++ Phase 2/front-end/src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
+import Header from '../Header/Header';
 
 function App() {
   return (
