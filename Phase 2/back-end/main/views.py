52b79c9cdc44416e0fb4fb50534e000c2fb3085e
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2023-11-20 16:44:55 -0500

    remove package.json files

--- Phase 2/back-end/main/views.py
+++ Phase 2/back-end/main/views.py
@@ -3,6 +3,26 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import sqlite3
 
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+
+            # Authenticate the user
+            user = login_helper(username=username, password=password)
+
+            # Username and password are correct
+            if user==True:
+                return JsonResponse({'success': True, 'message': 'Login successful'}, status=200)
+            else:
+                return JsonResponse({'success': False, 'message': 'Invalid credentials'}, status=403)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'message': 'Invalid JSON data'}, status=400)
+    
+    return JsonResponse({'success': False, 'message': 'Invalid request method'}, status=405)
 
 def login_helper(username, password):
     con = sqlite3.connect('helpdesk.db')
@@ -26,26 +46,4 @@ def login_helper(username, password):
         else:
             con.close()
             return False
-        
     
-    
-@csrf_exempt
-def login(request):
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            username = data.get('username')
-            password = data.get('password')
-
-            # Authenticate the user
-            user = login_helper(username=username, password=password)
-
-            # Username and password are correct
-            if user==True:
-                return JsonResponse({'success': True, 'message': 'Login successful'}, status=200)
-            else:
-                return JsonResponse({'success': False, 'message': 'Invalid credentials'}, status=403)
-        except json.JSONDecodeError:
-            return JsonResponse({'success': False, 'message': 'Invalid JSON data'}, status=400)
-    
-    return JsonResponse({'success': False, 'message': 'Invalid request method'}, status=405)
