1858d0fc30e09d8b314b99619b52e0d430c8bc13
allenAbi <a1allen@ryerson.ca>
2023-11-24 02:18:52 -0500

    Finish customer dashboard styline

--- Phase 2/front-end/src/components/CustomerDashboard/CustomerDashboard.js
+++ Phase 2/front-end/src/components/CustomerDashboard/CustomerDashboard.js
@@ -16,20 +16,41 @@ export default function CustomerDashboard() {
                 <div className="customer-first-option">
                     <img className="customer-icons" src={compose_icon} alt='Compose' />
                     <div className="customer-option-text-container">
-                        <span className="options-titlecase">Create Ticket</span>
+                        <div className="options-titlecase">Create Ticket</div>
                         Request IT services for issues you are facing
                     </div>
                 </div>
                 <div className="customer-second-option">
                     <img className="customer-icons" src={inbox_icon} alt='Inbox' />
                     <div className="customer-option-text-container">
-                        <span className="options-titlecase">View My Tickets</span>
+                        <div className="options-titlecase">View My Tickets</div>
                         Get a detailed list of all your open tickets
                     </div>
                 </div>
             </div>
             <div className="customer-faq-card">
-                FAQ
+                <span className="faq-titlecase">FAQ</span>
+                <div className="customer-faq-card-container">
+                    <div className="customer-faq-card-section">
+                        <div className="faq-questioncase">
+                            How will I know if there are any changes to my appointment?
+                        </div>  
+                        <div>
+                            You will receive a notification in your email if any changes 
+                            have been made to your appointment after it has been created. 
+                        </div>
+                    </div>
+                    <div className="customer-faq-card-section">
+                        <div className="faq-questioncase">
+                            <div>I am not able to make my appointment.</div>
+                            <div>Can I reschedule?</div>
+                        </div>
+                        <div>
+                            Yes, if you click on your ticket in ‘View My Tickets’, you will 
+                            have the option to edit ticket details.
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
