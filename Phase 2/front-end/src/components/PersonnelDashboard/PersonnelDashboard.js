c6f4b70fdd222d33c538b03f15ade5a459dbac20
allenAbi <a1allen@ryerson.ca>
2023-11-24 17:25:16 -0500

    Add style to personnel-dashboard

--- Phase 2/front-end/src/components/PersonnelDashboard/PersonnelDashboard.js
+++ Phase 2/front-end/src/components/PersonnelDashboard/PersonnelDashboard.js
@@ -3,6 +3,8 @@ import styles from './personnel-dashboard-styles.css';
 import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 import personnel_user_icon from '../../assets/personnel-dashboard-user-icon.svg';
+import personnel_mail_icon from '../../assets/personnel-dashboard-mail-icon.svg';
+import personnel_phone_icon from '../../assets/personnel-dashboard-phone-icon.svg';
 
 export default function PersonnelDashboard() {
   return (
@@ -10,6 +12,23 @@ export default function PersonnelDashboard() {
         <div className="personnel-dashboard-info-card">
             <div className="personnel-dashboard-info-title">
                 Agent Information
+            </div>
+            <div className="personnel-dashboard-info-details-container">
+                <img className="personnel_user_icon-style" src={personnel_user_icon} alt='User Icon' />
+                <div>
+                    <div id="personnel-dashboard-details-name">Jane Doe</div>
+                    <div>IT Specialist</div>
+                </div>
+                <div>
+                    <div className="personnel-dashboard-details-email-style">
+                        <img className="personnel_info_contact_icon" src={personnel_mail_icon} alt='Email' />
+                        <span id="personnel-dashboard-details-email">janedoe@aaier.com</span>
+                    </div>
+                    <div className="personnel-dashboard-details-phone-number-style">
+                        <img className="personnel_info_contact_icon" src={personnel_phone_icon} alt='Phone' />
+                        <span id="personnel-dashboard-details-phone-number">(416) 123-4567</span>
+                    </div>
+                </div>
             </div>
         </div>
         <div className="personnel-dashboard-tickets-card">
