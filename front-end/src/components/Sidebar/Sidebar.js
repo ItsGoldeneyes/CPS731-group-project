5ad9eddbf8b45c406da919a02b7f0437bfd4f439
mlec01 <ylimechiu@gmail.com>
2023-12-03 23:04:45 -0500

    add logout function

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -2,11 +2,18 @@ import React from 'react'
 import './sidebar-styles.css';
 
 export default function Sidebar() {
+
+  const logout = () => {
+    localStorage.removeItem('user_id')
+    window.location.href = '/'
+  }
+
   return (
     <div className="sidebar-element">
         <div className="sidebar-icons-container"> 
             <button className="sidebar-home-icon-style" type='button' onClick={() => window.location.href='/home'} alt='Home Button'></button>
             <button className="sidebar-list-icon-style" type='button' onClick={() => window.location.href='/view-all-tickets'} alt='List Tickets'></button>
+            <button className="sidebar-logout-icon-style" type='button' onClick={logout} alt='Logout'></button>
         </div>
     </div>
   )
